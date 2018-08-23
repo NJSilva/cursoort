348cb6155b76d72d3a0caa1b33aeb4b41cdca0cc
Romina Pons <rpons23@gmail.com>
2018-08-23 13:06:03 -0300

    cambio al obtener datos

--- src/main/java/src/test/TestFacadeClienteTiposVO.java
+++ src/main/java/src/test/TestFacadeClienteTiposVO.java
@@ -24,33 +24,33 @@ public class TestFacadeClienteTiposVO {
 
         FacadeCliente fc = new FacadeCliente();
 
-//        try {
-//            List<VOI> tipos = fc.obtener(FacadeCliente.TIPOS);
-//            
-//            for (Iterator<VOI> iterator = tipos.iterator(); iterator.hasNext();) {
-//                TiposVO next = (TiposVO) iterator.next();
-//                System.out.println(next.getTipos_id());
-//                System.out.println(next.getTipos_nombre());
-//            }
-//        } catch (SQLException ex) {
-//            Logger.getLogger(TestFacadeClienteTiposVO.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        
-        /* TIPOS */
-        TiposVO tiposvo = new TiposVO();
-//        tiposvo.setTipos_id(12);
-        tiposvo.setTipos_nombre("Nombre diez");
-
         try {
-            fc.grabar(tiposvo, FacadeCliente.TIPOS);
-//            fc.eliminar(tiposvo, FacadeCliente.TIPOS);
-//            fc.grabar(tiposvo, FacadeCliente.TIPOS);
-
-//            tiposvo.setTipos_nombre("Nombre 10");
-//            fc.actualizar(tiposvo, FacadeCliente.TIPOS);
+            List<VOI> tipos = fc.obtener(FacadeCliente.TIPOS);
+            
+            for (Iterator<VOI> iterator = tipos.iterator(); iterator.hasNext();) {
+                TiposVO next = (TiposVO) iterator.next();
+                System.out.println(next.getTipos_id());
+                System.out.println(next.getTipos_nombre());
+            }
         } catch (SQLException ex) {
             Logger.getLogger(TestFacadeClienteTiposVO.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        /* TIPOS */
+//        TiposVO tiposvo = new TiposVO();
+//        tiposvo.setTipos_id(12);
+//        tiposvo.setTipos_nombre("Nombre diez");
+//
+//        try {
+////            fc.grabar(tiposvo, FacadeCliente.TIPOS);
+////            fc.eliminar(tiposvo, FacadeCliente.TIPOS);
+////            fc.grabar(tiposvo, FacadeCliente.TIPOS);
+//
+////            tiposvo.setTipos_nombre("Nombre 10");
+////            fc.actualizar(tiposvo, FacadeCliente.TIPOS);
+//        } catch (SQLException ex) {
+//            Logger.getLogger(TestFacadeClienteTiposVO.class.getName()).log(Level.SEVERE, null, ex);
+//        }
 
 //        /* LIBROS */
 //        LibrosVO libro = new LibrosVO();
