5f4c3b84cc7ed6fdfe23d66bf40df7c82fd5efa5
Nico <Nico@LAPTOP-OKECKK60>
2018-09-02 18:21:09 -0300

    ns

--- BaseDeDatos/ProyectoORT/ProyectoDatosORT/InsertarDatos.sql
+++ BaseDeDatos/ProyectoORT/ProyectoDatosORT/InsertarDatos.sql
@@ -27,3 +27,41 @@ IN @TIPOS_ID DECIMAL(5,0)
 */
 
 call esquema.sp_libros_insert('1','El principito','35859685214',1960,'Descripcion del libro',15,'elprincipito_antoinedesaintexupery.jpg','Antoine de Saint-Exupery',41);
+
+
+-- PERSONAS --
+/*
+Create or Replace Procedure ESQUEMA.SP_Personas_Insert(
+--Campos de la tabla 
+IN @PERSONAS_CEDULA VARCHAR(10),
+IN @PERSONAS_MAIL VARCHAR(50),
+IN @PERSONAS_NOMBRE VARCHAR(120)
+)
+*/
+
+call esquema.sp_personas_insert('19716428','njsilva@gmail.com','Nicolas Silva');
+call esquema.sp_personas_insert('12345678','rpons@gmail.com','Romina Pons');
+
+
+-- PRESTAMOS --
+/*
+Create or Replace Procedure ESQUEMA.SP_Prestamos_Insert(
+--Campos de la tabla 
+IN P_LIBROS_ID DECIMAL(5,0),
+IN P_PERSONAS_ID DECIMAL(5,0),
+IN P_PRESTAMOS_FECHA_DESDE DATE,
+IN P_PRESTAMOS_FECHA_HASTA DATE
+)
+*/
+
+call esquema.sp_prestamos_insert(81,1,'2018-08-31',null);
+
+
+
+
+
+
+
+
+
+
