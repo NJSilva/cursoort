f1897733b634f7e0cbb8f1b8b0fdffd3edd8825b
Nicolas Silva <nsilva@cjpb.org.uy>
2018-09-26 18:24:33 -0300

    agregado

--- mobileangular/js/biblioteca.js
+++ mobileangular/js/biblioteca.js
@@ -2,15 +2,6 @@
     Defino el modulo para el sistema
 */
 
-var user = {
-    valido: 'no',
-    name: '',
-    clave: '',
-    email: '',
-    phone: '',
-    address: ''
-  };
-
 
 var modbiblioteca = angular.module('modbiblioteca' , ['ngRoute' , 'ngMaterial']);
 
@@ -19,8 +10,8 @@ modbiblioteca.config(function($locationProvider,$routeProvider){
     $locationProvider.html5Mode(true);
 
         $routeProvider
-        .when('/',{templateUrl : 'vistas/login.html'}) // Cuando se inicia se va al home directo porque la url tiene la barra
-        .when('/misreservas',{templateUrl:'vistas/misreservas.html'} 
+        .when('/'           ,{templateUrl : 'vistas/login.html'}) // Cuando se inicia se va al home directo porque la url tiene la barra
+        .when('/misreservas',{templateUrl : 'vistas/misreservas.html'} 
     )
 });
 
@@ -31,14 +22,43 @@ modbiblioteca.controller("ctlindex" , function($scope){
 
 
 /* Controlador login */
-modbiblioteca.controller("ctllogin" , function($scope){
+modbiblioteca.controller("ctllogin" , function($scope , $http){
 
-    $scope.hola = 'hola Nico';
+    // El usuario que esta logueado en el sistema
+    $scope.user={};
 
-      $scope.clicked = function() {
-          // user.valido = 'si';
-          alert('boton ingresar');
-      };
+    $scope.user.valid='no'; 
+    $scope.user.name=''; 
+    $scope.user.password=''; 
+    $scope.user.email='';
+    $scope.user.phone='';
+    $scope.user.adress='';
+
+    $scope.clicked = function() {
+        console.log($scope.user.name);
+        console.log($scope.user.password);
+        $scope.user.valid = 'si';
+
+        // Hacer la llamada al servidor y validar el usuario
+        /*
+        {	"login_clave": 12345678,
+            "personas": {
+                "personas_cedula": "19716428"
+	        }
+        }
+        */
+        $http({
+            method : 'POST',
+            crossDomain: true,
+            url : 'http://192.168.111.29:8080/BibliotecaORT/webresources/login',
+            data :  "{\t\"login_clave\": 12345678,\r\n\t\"personas\": {\r\n\t\t\"personas_cedula\": \"19716428\"\r\n\t}\r\n}",
+            headers : {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer 2018-cjpb",                
+            }
+        });        
+
+    };
    
 });
 
